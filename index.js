048c28c3bad225b7075c687f7b4dd0eb18faad37
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2022-08-18 12:31:08 +0300

    init

---
+++ index.js
@@ -0,0 +1,147 @@
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const bodyParser = require('body-parser');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+const app = express();
+const server = http.createServer(app);
+app.use(cors());
+app.use(bodyParser.json());
+
+let clients = [];
+
+app.get('/api', (req, res) => {
+  res.send({ status: 'Server is working!' });
+});
+
+app.get('/api/createClient', async (req, res) => {
+  const clientId = req.query.client;
+  const isClient = clients.find(x => x.clientId === clientId);
+  if (typeof clientId === 'string') {
+    if (isClient) {
+      try {
+        isClient.client.destroy();
+        const index = clients
+          .map(x => {
+            return x.clientId;
+          })
+          .indexOf(clientId);
+        clients.splice(index, 1);
+      } catch (err) {
+        res.send({ err: 'server error' });
+      }
+    }
+    try {
+      const client = new Client({
+        authStrategy: new LocalAuth({ clientId: clientId, dataPath: './sessions' }),
+        // puppeteer: { executablePath: '/usr/bin/chromium-browser', args: ['--no-sandbox'] },
+      });
+      client.initialize();
+      clients.push({ clientId: clientId, client: client });
+      console.log('client initializing...');
+      client.on('qr', qr => {
+        console.log('QR updated...');
+        res.send({ qr: qr });
+      });
+    } catch (err) {
+      res.send({ err: 'server error' });
+    }
+  } else {
+    res.send({ err: `${clientId} problem!` });
+  }
+});
+
+app.get('/api/getClient', async (req, res) => {
+  const clientId = req.query.client;
+  const client = clients.find(x => x.clientId === clientId);
+  if (client) {
+    try {
+      const clientInfo = await client.client.info;
+      res.send(clientInfo);
+    } catch (err) {
+      res.send({ err: 'server error' });
+    }
+  } else {
+    res.send({ err: `${client} not found!` });
+  }
+});
+
+app.get('/api/logout', (req, res) => {
+  const clientId = req.query.client;
+  const client = clients.find(x => x.clientId === clientId);
+  if (client) {
+    try {
+      client.client.destroy();
+      const index = clients
+        .map(x => {
+          return x.clientId;
+        })
+        .indexOf(clientId);
+      clients.splice(index, 1);
+      res.send({ client: 'logged out' });
+    } catch (err) {
+      res.send({ err: 'server error' });
+    }
+  } else {
+    res.send({ err: `${client} not found!` });
+  }
+});
+
+app.get('/api/getChats', async (req, res) => {
+  const clientId = req.query.client;
+  const client = clients.find(x => x.clientId === clientId);
+  if (client) {
+    try {
+      const chats = await client.client.getChats();
+      res.send(chats);
+    } catch (err) {
+      res.send({ err: 'server error' });
+    }
+  } else {
+    res.send({ err: `${client} not found!` });
+  }
+});
+
+app.get('/api/sendmessage', async (req, res, next) => {
+  const clientId = req.query.client;
+  const client = clients.find(x => x.clientId === clientId);
+  if (client) {
+    try {
+      const number = req.query.number;
+      const message = req.query.message;
+      if (typeof number === 'string' && typeof message === 'string') {
+        const msg = await client.client.sendMessage(`${number}@c.us`, message);
+        res.send({ msg });
+      }
+    } catch (err) {
+      res.send({ err: 'server error' });
+    }
+  } else {
+    res.send({ err: `${client} not found!` });
+  }
+});
+
+app.get('/api/getmessages', async (req, res, next) => {
+  const clientId = req.query.client;
+  const client = clients.find(x => x.clientId === clientId);
+  if (client) {
+    try {
+      const chatId = req.query.chatId;
+      if (typeof chatId === 'string') {
+        const chat = await client.client.getChatById(chatId);
+        const messages = await chat.fetchMessages({ limit: 30 });
+        res.send(messages);
+      } else {
+        res.send({ error: 'Unknown chatId' });
+      }
+    } catch (err) {
+      res.send({ err: 'server error' });
+    }
+  } else {
+    res.send({ err: `${client} not found!` });
+  }
+});
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
