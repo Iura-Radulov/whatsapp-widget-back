14f49116af0e046bfeeadf1f123fffeb2736a8d0
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2022-08-19 16:16:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,7 @@ const PORT = process.env.PORT || 8001;
 
 const app = express();
 
-app.use(
-  cors({
-    origin: '*',
-    credentials: true,
-  })
-);
+app.use(cors());
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', '*');
 //   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
