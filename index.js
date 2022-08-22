f50c1e79373f2e394056135c1ce16c606be402ef
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2022-08-22 22:24:36 +0300

    add

--- index.js
+++ index.js
@@ -1,36 +1,22 @@
 const express = require('express');
 const cors = require('cors');
-// const http = require('http');
-// const bodyParser = require('body-parser');
+const http = require('http');
+const bodyParser = require('body-parser');
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 require('dotenv').config();
 const path = require('path');
 
-const qrcode = require('qrcode-terminal');
-
 const PORT = process.env.PORT || 8001;
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(cors());
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
 
-// app.use(
-//   cors({
-//     origin: 'https://iura-radulov.github.io/whatsapp-widget/',
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     preflightContinue: false,
-//     optionsSuccessStatus: 204,
-//   })
-// );
-app.use(express.json());
-// app.use(bodyParser.json());
+// app.use(express.json());
+app.use(bodyParser.json());
 
 let clients = [];
 
@@ -43,37 +29,41 @@ app.get('/', (req, res) => {
 
 app.get('/api/createClient', async (req, res) => {
   const clientId = req.query.client;
-  // const isClient = clients.find(x => x.clientId === clientId);
+  const isClient = clients.find(x => x.clientId === clientId);
   if (typeof clientId === 'string') {
-    // if (isClient) {
-    //   try {
-    //     isClient.client.destroy();
-    //     const index = clients
-    //       .map(x => {
-    //         return x.clientId;
-    //       })
-    //       .indexOf(clientId);
-    //     clients.splice(index, 1);
-    //   } catch (err) {
-    //     res.json({ err: 'server error' });
-    //   }
-    // }
+    if (isClient) {
+      try {
+        isClient.client.destroy();
+        const index = clients
+          .map(x => {
+            return x.clientId;
+          })
+          .indexOf(clientId);
+        clients.splice(index, 1);
+      } catch (err) {
+        res.json({ err: 'server error' });
+      }
+    }
     try {
       const client = new Client({
         authStrategy: new LocalAuth({ clientId: clientId, dataPath: './sessions' }),
+        puppeteer: {
+          args: ['--no-sandbox'],
+        },
         // authStrategy: new LocalAuth(),
         // puppeteer: { executablePath: '/usr/bin/chromium-browser', args: ['--no-sandbox'] },
       });
-      // client.initialize();
+      client.initialize();
       clients.push({ clientId: clientId, client: client });
       console.log('client initializing...');
       client.on('qr', qr => {
         console.log('QR updated...');
         res.json({ qr: qr });
       });
-
-      client.initialize();
-      // qrcode.generate(qr, { small: true });
+      client.on('ready', () => {
+        console.log('Client is ready!');
+      });
+      // client.initialize();
     } catch (err) {
       res.json({ err: 'server error' });
     }
@@ -140,21 +130,20 @@ app.get('/api/sendmessage', async (req, res, next) => {
     try {
       const number = req.query.number;
       const message = req.query.message;
-      const type = req.query.type;
-      if (typeof number === 'string' && typeof message === 'string' && type === 'image/png') {
-        const productPath = path.join(__dirname, message.name);
-        const media = MessageMedia.fromFilePath(productPath);
-        const chat = await client.client.getChats();
-        chat.sendMessage(media);
 
-        if (typeof number === 'string' && typeof message === 'string') {
-          const msg = await client.client.sendMessage(`${number}@c.us`, message);
-          res.json({ msg });
-        }
+      // if (typeof number === 'string' && typeof message === 'string' && type === 'image/png') {
+      //   const productPath = path.join(__dirname, message.name);
+      //   const media = MessageMedia.fromFilePath(productPath);
+      //   const chat = await client.client.getChats();
+      //   chat.sendMessage(media);
 
-        // const msg = await client.client.sendMessage(`${number}@c.us`, productPath);
-        // res.json({ msg });
+      if (typeof number === 'string' && typeof message === 'string') {
+        const msg = await client.client.sendMessage(`${number}@c.us`, message);
+        res.json({ msg });
       }
+
+      // const msg = await client.client.sendMessage(`${number}@c.us`, productPath);
+      // res.json({ msg });
     } catch (err) {
       res.json({ err: 'server error' });
     }
@@ -189,15 +178,25 @@ app.get('/api/sendmedia', async (req, res, next) => {
   const client = clients.find(x => x.clientId === clientId);
   if (client) {
     try {
-      const number = req.query.number;
-      const message = req.query.message;
-      if (typeof number === 'string' && typeof message === 'string') {
-        const productPath = path.join(__dirname, message.name);
-        console.log(productPath);
-        const media = MessageMedia.fromFilePath(productPath);
-        const chat = await client.client.getChats();
-        chat.sendMessage(media);
-      }
+      // const number = req.query.number;
+      // const message = req.query.message;
+      client.on('message', async msg => {
+        if (msg.hasMedia) {
+          const media = await msg.downloadMedia();
+          res.json(media);
+          // const chat = await client.client.getChats();
+          // chat.sendMessage(media);
+        }
+      });
+
+      // if (typeof number === 'string' && typeof message === 'string') {
+      //   const productPath = path.join(__dirname, message.name);
+      //   console.log(productPath);
+      //   res.json(productPath);
+      //   const media = MessageMedia.fromFilePath(productPath);
+      //   const chat = await client.client.getChats();
+      //   chat.sendMessage(media);
+      // }
     } catch (err) {
       res.json({ err: 'server error' });
     }
@@ -206,4 +205,4 @@ app.get('/api/sendmedia', async (req, res, next) => {
   }
 });
 
-app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
